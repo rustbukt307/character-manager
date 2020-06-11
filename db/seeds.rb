e6e6bc35cd49cfb354f4ec21ac2808444525fa53
Tom Moliterno <rustbukt307@Toms-MacBook-Air.local>
2020-06-10 22:24:30 -0400

    seed coded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Campaign.destroy_all
+Character.destroy_all
+User.destroy_all
+
+@user = User.create!({ username: 'testeban2020', password: '123456' })
+
+p "#{User.count} user(s) created"
+
+@characteronius = Character.create!(name: 'Characteronius the Mighty')
+
+p "#{Character.count} Character(s) created"
+
+Campaign.create!(name: 'Campaign 1', user: @user, characters: [@characteronius])
+
+p "#{Campaign.count} campaign(s) created"

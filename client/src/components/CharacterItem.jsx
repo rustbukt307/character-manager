5640b0c326cb4e86d15d59e684fbe1240399c996
Tom Moliterno <rustbukt307@Toms-MacBook-Air.local>
2020-06-15 19:31:57 -0400

    character sheet built and rendered

--- client/src/components/CharacterItem.jsx
+++ client/src/components/CharacterItem.jsx
@@ -23,7 +23,25 @@ class CharacterItem extends Component {
       <div>
         {currentCharacter && (
           <>
-            <div>{currentCharacter.name}</div>
+            <h3>{currentCharacter.name}</h3>
+            <h4>Race: </h4><p>{currentCharacter.race}</p>
+            <h4>Class: </h4><p>{currentCharacter.char_class}</p>
+            <h4>Level: </h4><p>{currentCharacter.level}</p>
+            <h4>Alignment: </h4><p>{currentCharacter.alignment}</p>
+            <h4>Experience Pts: </h4><p>{currentCharacter.xp}</p>
+            <h4>Strength: </h4><p>{currentCharacter.str}</p>
+            <h4>Dexterity: </h4><p>{currentCharacter.dex}</p>
+            <h4>Constitution: </h4><p>{currentCharacter.con}</p>
+            <h4>Intelligence: </h4><p>{currentCharacter.int}</p>
+            <h4>Wisdom: </h4><p>{currentCharacter.wis}</p>
+            <h4>Charisma: </h4><p>{currentCharacter.cha}</p>
+            <h4>Proficiency Bonus: </h4><p>{currentCharacter.prof_bonus}</p>
+            <h4>Current Hit Points: </h4><p>{currentCharacter.hp}</p>
+            <h4>Armor Class: </h4><p>{currentCharacter.ac}</p>
+            <h4>Hit Dice: </h4><p>{currentCharacter.hit_dice}</p>
+            <h4>Attacks & Casting: </h4><p>{currentCharacter.attk_cast}</p>
+            <h4>Inventory: </h4><p>{currentCharacter.inventory}</p>
+            <h4>Skills & Feats: </h4><p>{currentCharacter.feats}</p>
           </>
         )}
       </div>

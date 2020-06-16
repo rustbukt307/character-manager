b1f2232bb75d9c9c83bff9a60f1848fe9b27ae65
Tom Moliterno <rustbukt307@Toms-MacBook-Air.local>
2020-06-15 21:13:30 -0400

    styling in header fixed, character sheet built out

--- client/src/components/CharacterItem.jsx
+++ client/src/components/CharacterItem.jsx
@@ -20,28 +20,64 @@ class CharacterItem extends Component {
   render() {
     const {currentCharacter} = this.state
     return (
-      <div>
+      <div className="char-sheet">
         {currentCharacter && (
           <>
-            <h3>{currentCharacter.name}</h3>
-            <h4>Race: </h4><p>{currentCharacter.race}</p>
-            <h4>Class: </h4><p>{currentCharacter.char_class}</p>
-            <h4>Level: </h4><p>{currentCharacter.level}</p>
-            <h4>Alignment: </h4><p>{currentCharacter.alignment}</p>
-            <h4>Experience Pts: </h4><p>{currentCharacter.xp}</p>
-            <h4>Strength: </h4><p>{currentCharacter.str}</p>
-            <h4>Dexterity: </h4><p>{currentCharacter.dex}</p>
-            <h4>Constitution: </h4><p>{currentCharacter.con}</p>
-            <h4>Intelligence: </h4><p>{currentCharacter.int}</p>
-            <h4>Wisdom: </h4><p>{currentCharacter.wis}</p>
-            <h4>Charisma: </h4><p>{currentCharacter.cha}</p>
-            <h4>Proficiency Bonus: </h4><p>{currentCharacter.prof_bonus}</p>
-            <h4>Current Hit Points: </h4><p>{currentCharacter.hp}</p>
-            <h4>Armor Class: </h4><p>{currentCharacter.ac}</p>
-            <h4>Hit Dice: </h4><p>{currentCharacter.hit_dice}</p>
-            <h4>Attacks & Casting: </h4><p>{currentCharacter.attk_cast}</p>
-            <h4>Inventory: </h4><p>{currentCharacter.inventory}</p>
-            <h4>Skills & Feats: </h4><p>{currentCharacter.feats}</p>
+            <h3 className="char-name">{currentCharacter.name}</h3>
+            <div className="char-details">
+              <h4>Experience Pts: </h4>
+              <p>{currentCharacter.xp}</p>
+              <h4>Level: </h4>
+              <p>{currentCharacter.level}</p>
+              <h4>Race: </h4>
+              <p>{currentCharacter.race}</p>
+              <h4>Class: </h4>
+              <p>{currentCharacter.char_class}</p>
+              <h4>Alignment: </h4>
+              <p>{currentCharacter.alignment}</p>
+            </div>
+            <div className="stats">
+              <h4>Strength: </h4>
+              <p>{currentCharacter.str}</p>
+              <h4>Dexterity: </h4>
+              <p>{currentCharacter.dex}</p>
+              <h4>Constitution: </h4>
+              <p>{currentCharacter.con}</p>
+              <h4>Intelligence: </h4>
+              <p>{currentCharacter.int}</p>
+              <h4>Wisdom: </h4>
+              <p>{currentCharacter.wis}</p>
+              <h4>Charisma: </h4>
+              <p>{currentCharacter.cha}</p>
+            </div>
+            <div className="prof-bonus">
+              <h4>Proficiency Bonus: </h4>
+              <p>{currentCharacter.prof_bonus}</p>
+            </div>
+            <div className="hp">
+              <h4>Current Hit Points: </h4>
+              <p>{currentCharacter.hp}</p>
+            </div>
+            <div className="ac">
+              <h4>Armor Class: </h4>
+              <p>{currentCharacter.ac}</p>
+            </div>
+            <div className="hit-dice">
+              <h4>Hit Dice: </h4>
+              <p>{currentCharacter.hit_dice}</p>
+            </div>
+            <div className="attk-cast">
+              <h4>Attacks & Casting: </h4>
+              <p>{currentCharacter.attk_cast}</p>
+            </div>
+            <div className="inventory">
+              <h4>Inventory: </h4>
+              <p>{currentCharacter.inventory}</p>
+            </div>
+            <div className="feats">
+              <h4>Skills & Feats: </h4>
+              <p>{currentCharacter.feats}</p>
+            </div>
           </>
         )}
       </div>
